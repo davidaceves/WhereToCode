d1b8e46048d2a8313ed1efdd78583321a0f3628e
David Aceves <daceves.dev@gmail.com>
2021-04-12 23:02:57 -0400

    added more styled components, major changes to searchbar styling

--- client/src/components/Map/SearchBar.jsx
+++ client/src/components/Map/SearchBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRef } from "react";
 
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const SearchBar = (props) => {
   const searchButton = useRef(null);
@@ -12,17 +12,30 @@ const SearchBar = (props) => {
 
   return (
     <Container>
-      <InputField
-        id="locationType"
-        placeholder="What are you looking for...ex: cafe"
-        onChange={props.onChange}
-        value={props.query}
-      />
-      <InputField
-        id="autocomplete"
-        placeholder="Enter location..."
-        onFocus={props.onFocus}
-      />
+      <FormGroup>
+        <FormInputContainer>
+          <InputField
+            id="place"
+            placeholder="Place"
+            name="place"
+            onChange={props.onChange}
+            value={props.query}
+            tabIndex="0"
+          />
+          <FormLabel for="place">Place</FormLabel>
+        </FormInputContainer>
+        <FormInputContainer>
+          <InputFieldLocation
+            id="locationAutoComplete"
+            placeholder="Location"
+            name="locationAutoComplete"
+            tabIndex="1"
+          />
+          <FormLabelLocation for="locationAutoComplete">
+            Location
+          </FormLabelLocation>
+        </FormInputContainer>
+      </FormGroup>
       <Button ref={searchButton} onClick={addEventListener}>
         Search
       </Button>
@@ -30,26 +43,122 @@ const SearchBar = (props) => {
   );
 };
 
+// #TODO Button is taking two clicks to work
+
 export default SearchBar;
 
 // Not sure what I was doing here...
 // props.locationLen !== 0 ? 49vw :
 
 const Container = styled.div`
-  width: 100%;
+  font-family: Poppins, sans-serif;
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
+  height: 25%;
+  font-size: 1.5rem;
+`;
+
+const FormInputContainer = styled.div`
+  margin-bottom: 50px;
+`;
+
+const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  padding: 15px 0 0;
+  margin-top: 10px;
+  width: 50%;
+`;
+
+const FormLabel = styled.label`
+  position: absolute;
+  top: 0;
+  display: block;
+  transition: 0.2s;
+  font-size: 1rem;
+  color: gray;
+`;
+
+const FormLabelLocation = styled.label`
+  position: absolute;
+  top: 88px;
+  display: block;
+  transition: 0.2s;
+  font-size: 1rem;
+  color: gray;
 `;
 
 const InputField = styled.input`
-  position: relative;
-  border: 1px solid black;
-  borderbottom: 1px solid black;
-  width: 50%;
-  height: 30px;
-  margin: 2%;
-  fontsize: 20px;
+  font-family: inherit;
+  width: 100%;
+  border: 0;
+  border-bottom: 2px solid gray;
+  outline: 0;
+  font-size: 1rem;
+  padding: 7px 0;
+  background: transparent;
+  transition: border-color 0.2s;
+
+  &::placeholder {
+    color: transparent;
+  }
+
+  &:placeholder-shown ~ ${FormLabel} {
+    font-size: 1.3rem;
+    cursor: text;
+    top: 27px;
+  }
+
+  :focus {
+    ~ ${FormLabel} {
+      position: absolute;
+      top: 0;
+      display: block;
+      transition: 0.2s;
+      font-size: 1rem;
+      color: black;
+      font-weight: 700;
+    }
+  }
+`;
+
+const InputFieldLocation = styled.input`
+  font-family: inherit;
+  width: 100%;
+  border: 0;
+  border-bottom: 2px solid gray;
+  outline: 0;
+  font-size: 1rem;
+  padding: 7px 0;
+  background: transparent;
+  transition: border-color 0.2s;
+  -webkit-user-select: all;
+  user-select: all;
+
+  &::placeholder {
+    color: transparent;
+  }
+
+  &:placeholder-shown ~ ${FormLabelLocation} {
+    font-size: 1.3rem;
+    cursor: text;
+    top: 115px;
+  }
+
+  :focus {
+    ~ ${FormLabelLocation} {
+      position: absolute;
+      top: 88px;
+      display: block;
+      transition: 0.2s;
+      font-size: 1rem;
+      color: black;
+      font-weight: 700;
+    }
+  }
 `;
 
 const Button = styled.button`
