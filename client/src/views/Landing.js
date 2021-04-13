3113fc788087e2bdd94dd24541ede2abe2e76e85
David Aceves <daceves.dev@gmail.com>
2021-04-12 23:04:47 -0400

    mostly prettier changes and padding change

--- client/src/views/Landing.js
+++ client/src/views/Landing.js
@@ -7,14 +7,14 @@ import styled from "styled-components";
 
 import { withRouter, Link } from "react-router-dom";
 
-import mockup from '../assets/mockup.png';
-import hours from '../assets/hours.jpg';
-import explore from '../assets/explore.jpg';
-import reviews from '../assets/reviews.jpg';
+import mockup from "../assets/mockup.png";
+import hours from "../assets/hours.jpg";
+import explore from "../assets/explore.jpg";
+import reviews from "../assets/reviews.jpg";
 
 import * as ROUTES from "../Routes/routes";
 
-const Landing = props => {
+const Landing = (props) => {
   const [currentActivity, setCurrentActivity] = useState("code");
   const [number, setNumber] = useState(1);
 
@@ -37,7 +37,7 @@ const Landing = props => {
       "geometry",
       "icon",
       "name",
-      "place_id"
+      "place_id",
     ]);
     autocomplete.addListener("place_changed", () => {
       props.setPlace(autocomplete.getPlace());
@@ -52,48 +52,76 @@ const Landing = props => {
     <LandingPageContainer>
       <Navigation />
       <LandingScreen>
-      <SearchComponent>
-        <h2>
-          Find a place to <span>{currentActivity}</span> near you
-        </h2>
+        <SearchComponent>
+          <h2>
+            Find a place to <span>{currentActivity}</span> near you
+          </h2>
 
-        <InputAndButtonContainer>
-          <Input id="exploreAutoComplete" placeholder="Explore" size="40" />
-          <GoButton to={ROUTES.HOME}>Go</GoButton>
-        </InputAndButtonContainer>
-      </SearchComponent>
+          <InputAndButtonContainer>
+            <Input id="exploreAutoComplete" placeholder="Explore" size="40" />
+            <GoButton to={ROUTES.HOME}>Go</GoButton>
+          </InputAndButtonContainer>
+        </SearchComponent>
       </LandingScreen>
       <FeaturesInfo>
         <InfoBox>
           <h3>Work Easier</h3>
-          <p>Finding a place to work away form the home or office doesn't need to be a hassle. Find locations that meet your needs no matter where you are!</p>
+          <p>
+            Finding a place to work away form the home or office doesn't need to
+            be a hassle. Find locations that meet your needs no matter where you
+            are!
+          </p>
           <ExploreButton to={ROUTES.HOME}>Explore →</ExploreButton>
         </InfoBox>
         <FeatureBox>
           <FeaturePic src={explore} />
           <h3>Find New Places</h3>
-          <p>Whether you're traveling or looking to get out of the house in your own home, we've got you covered.</p>
+          <p>
+            Whether you're traveling or looking to get out of the house in your
+            own home, we've got you covered.
+          </p>
         </FeatureBox>
         <FeatureBox>
           <FeaturePic src={hours} />
           <h3>Get Info</h3>
-          <p>Find the location's available hours, address, phone number, and more.</p>
+          <p>
+            Find the location's available hours, address, phone number, and
+            more.
+          </p>
         </FeatureBox>
         <FeatureBox>
           <FeaturePic src={reviews} />
           <h3>Pick the Best One</h3>
-          <p>Read reviews from remote workers like you! Read about the speed and reliability of the internet, the noise level, and other important factors.</p>
+          <p>
+            Read reviews from remote workers like you! Read about the speed and
+            reliability of the internet, the noise level, and other important
+            factors.
+          </p>
         </FeatureBox>
       </FeaturesInfo>
       <MockupContainer>
-          <Mockup src={mockup} />
+        <Mockup src={mockup} />
         <LargeMarketing>
           <h3>Find Great Locations</h3>
-          <p>Whether you're looking for some free wifi to work while you're traveling or you're just wanting to get away from the house or office for a change of scenery, The HiveStack is built to help you find the best place to work.</p>
+          <p>
+            Whether you're looking for some free wifi to work while you're
+            traveling or you're just wanting to get away from the house or
+            office for a change of scenery, The HiveStack is built to help you
+            find the best place to work.
+          </p>
           <Bullets>
-            <div><i class="fa fa-coffee fa-2x"></i><p>Find coffee shops and cafes</p></div>
-            <div><i class="fas fa-wifi fa-2x"></i><p>Check wifi speeds and reliability</p></div>
-            <div><i class="fas fa-laptop fa-2x"></i><p>Sit down and get some work done</p></div>
+            <div>
+              <i class="fa fa-coffee fa-2x"></i>
+              <p>Find coffee shops and cafes</p>
+            </div>
+            <div>
+              <i class="fas fa-wifi fa-2x"></i>
+              <p>Check wifi speeds and reliability</p>
+            </div>
+            <div>
+              <i class="fas fa-laptop fa-2x"></i>
+              <p>Sit down and get some work done</p>
+            </div>
           </Bullets>
         </LargeMarketing>
       </MockupContainer>
@@ -114,11 +142,11 @@ const SearchComponent = styled.div`
     font-size: 48px;
     font-weight: normal;
 
-    span{
+    span {
       font-weight: bold;
     }
 
-    @media(max-width:500px){
+    @media (max-width: 500px) {
       font-size: 28px;
       width: 80%;
       text-align: center;
@@ -130,22 +158,21 @@ const SearchComponent = styled.div`
   }
 `;
 
-const LandingPageContainer = styled.div`
-`;
+const LandingPageContainer = styled.div``;
 
 const LandingScreen = styled.div`
-background: url("/heroimage.svg");
-background-size: cover;
-display: flex;
-justify-content: center;
-align-items: center;
-width: 100%;
-height: 100vh;
+  background: url("/heroimage.svg");
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
 
-@media(max-width: 500px){
-  height: 98vh;
-}
-`
+  @media (max-width: 500px) {
+    height: 98vh;
+  }
+`;
 
 const GoButton = styled(Link)`
   display: flex
@@ -158,7 +185,7 @@ const GoButton = styled(Link)`
   background: gold;
   border: 1px solid gold;
   border-left: none;
-  padding: 0 10px 0 10px;
+  padding: 10px 10px 0 10px;
   &:hover {
     background: yellow;
   }
@@ -174,7 +201,7 @@ const Input = styled.input`
     vertical-align: center;
     font-size: 1rem;
   }
-  @media(max-width: 500){
+  @media (max-width: 500) {
     width: 50px;
   }
 `;
@@ -187,10 +214,10 @@ const FeaturesInfo = styled.div`
   margin: 125px 2% 20px 2%;
   display: flex;
   justify-content: space-between;
-  @media(max-width: 500px){
+  @media (max-width: 500px) {
     flex-direction: column;
   }
-`
+`;
 
 const InfoBox = styled.div`
   width: 23%;
@@ -211,37 +238,37 @@ const InfoBox = styled.div`
     width: 95%;
     margin-bottom: 20px;
   }
-`
+`;
 
 const FeatureBox = styled(InfoBox)`
-  border: 1px solid #ECECEC;
+  border: 1px solid #ececec;
   h3 {
     font-size: 20px;
   }
   p {
     font-size: 14px;
   }
-`
+`;
 const FeaturePic = styled.img`
   width: 100%;
   height: 185px;
-`
+`;
 
 const MockupContainer = styled.div`
   display: flex;
   justify-content: space-around;
-  @media(max-width: 500px){
+  @media (max-width: 500px) {
     flex-direction: column;
   }
-`
+`;
 
 const Mockup = styled.img`
   width: 45%;
   margin: 50px 0;
-  @media(max-width: 500px){
+  @media (max-width: 500px) {
     width: 90%;
   }
-`
+`;
 const ExploreButton = styled(GoButton)`
   font-size: 1.1rem;
   border-radius: 10px;
@@ -249,12 +276,12 @@ const ExploreButton = styled(GoButton)`
   font-weight: bold;
   width: 125px;
   padding: 10px;
-`
+`;
 
 const LargeMarketing = styled(InfoBox)`
   width: 40%;
   margin-top: 80px;
-`
+`;
 
 const Bullets = styled.div`
   padding-left: 10px;
@@ -267,10 +294,10 @@ const Bullets = styled.div`
     i {
       padding-right: 20px;
     }
-    p{
+    p {
       margin-top: 0;
       color: black;
       font-size: 16px;
     }
   }
-`
+`;
